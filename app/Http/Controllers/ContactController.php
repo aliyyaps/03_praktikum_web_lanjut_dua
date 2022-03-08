d700970a50cdaa0d9e1839ed476a3a6cbecc34dd
= <=>
2022-03-08 09:43:53 +0700

    Change Contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     public function contact() {
-        return view('contact');
+        return view('contact')
+        ->with('title','Contact');
     }
 }
