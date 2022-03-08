feea31f2adde7281bfb89f5102bfed7023d749ab
= <=>
2022-03-08 09:43:13 +0700

    Change Home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,8 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function home()
-    {
-        return view('index');
+    public function home() {
+        return view('index')
+        ->with('title','Home');
     }
 }
