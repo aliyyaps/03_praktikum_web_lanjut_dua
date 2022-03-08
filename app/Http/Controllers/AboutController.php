b22a8e313397e3c1d161ed0c6d48fbcf891e3850
= <=>
2022-03-08 09:43:30 +0700

    Change About

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class AboutController extends Controller
 {
     public function about() {
-        return view('about');
+        return view('about')
+        ->with('title','About');
     }
 }
