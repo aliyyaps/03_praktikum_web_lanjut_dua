5b5c30e12ee12eebc227e5c75c280d444e818d05
= <=>
2022-03-09 14:22:06 +0700

    Menghapus Tag Penutup Body dan HTML

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -41,8 +41,8 @@
                         <ul>
                             <li><a href="/home" >Home</a></li>
                             <li><a href="/about">About</a></li>
-                            <li><a href="/profile">profile</a></li>
-                            <li><a href="/contact">contact</a></li>
+                            <li><a href="/profile">Profile</a></li>
+                            <li><a href="/contact">Contact</a></li>
                         </ul>
                     </nav>
                 </div>
