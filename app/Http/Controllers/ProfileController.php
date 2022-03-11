c0c59e08009663695495079e9976d192acc5ce91
= <=>
2022-03-11 08:31:39 +0700

    Model - Praktikum 2

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProfileController extends Controller
 {
     public function profile() {
-        return view('profile')
+        $data = Product::all();
+        return view('profile', ['daftar' => $data])
         ->with('title','Profile');
     }
 }
